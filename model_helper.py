c7e882bfd97a007a3cbb0a2e902e7d3698eec278
mimbres <sungkyun.chang@gmail.com>
2024-08-01 11:30:07 +0900

    .

--- model_helper.py
+++ model_helper.py
@@ -1,9 +1,4 @@
 # @title Model helper
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-
-
 from collections import Counter
 import argparse
 import torch
