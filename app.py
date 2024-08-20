54a1a79aed63ff4a02ba3e266180a2e7d94e129a
Sungkyun Chang <mimbres@users.noreply.huggingface.co>
2024-08-20 12:32:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,8 @@ def prepare_media(source_path_or_url: os.PathLike,
     if source_type == 'audio_filepath':
         audio_file = source_path_or_url
     elif source_type == 'youtube_url':
-        yt = YouTube(source_path_or_url)
+        proxy_handler = {"http": "http://127.0.0.1:1087", "https":"http://127.0.0.1:1087"}
+        yt = YouTube(source_path_or_url, proxies=proxy_handler)
         audio_stream = min(yt.streams.filter(only_audio=True), key=lambda s: s.bitrate)
         mp4_file = audio_stream.download(output_path='downloaded') # ./downloaded
         audio_file = mp4_file[:-3] + 'mp3'
